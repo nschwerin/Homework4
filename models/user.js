b0fd533059afe465c06712be59c34bad5682a439
Nicole <nschwerin@ltu.edu>
2021-04-28 13:32:40 -0400

    index

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const Mongoose = require('mongoose');
+
+module.exports = Mongoose.model('User', new Mongoose.Schema({
+    Name: {type: String, required: true, unique: true},
+}, {
+    toJSON: {
+        getters: true,
+        virtuals: false,
+    },
+}));
--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
+require('dotenv').config();
 const Mongoose = require('mongoose');
-const app = require('express')();
+const Express = require('express');
+
+const app = Express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const user = require('../models/user');
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: '.' });
+});
